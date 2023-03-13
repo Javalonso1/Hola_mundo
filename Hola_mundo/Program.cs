cee88d43f01a860f256117a002039e8217406828
Usuario Local <javalo08@ucm.es>
2023-03-13 12:19:55 +0100

    Prueba1

--- Hola_mundo/Program.cs
+++ Hola_mundo/Program.cs
@@ -4,13 +4,8 @@
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Hola, World!");
-            Console.WriteLine("Queso");
-=======
             Console.WriteLine("Hello, World!!");
             Console.WriteLine(":)");
->>>>>>> c90be7558d3559f23a9ecf1a0dd167246bf9fb81
         }
     }
 }
